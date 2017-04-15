5f7b044dab75abbe133371e62acf8859a22067ee
Mike Kiernan <mkiernan@microsoft.com>
2017-04-15 18:11:04 +0400

    add vmss extension

--- clusRun.sh
+++ clusRun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USER=`whoami`
-NAMES=`cat /home/$USER/bin/nodenames.txt` #names from names.txt file
+NAMES=`cat /share/home/$USER/bin/nodenames.txt` #names from names.txt file
 echo "launching $1"
 for NAME in $NAMES; do
   #the & here will fork off a run for each node and move to the next, the wait at the end waits until all is complete
