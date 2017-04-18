fbd2e8f4d972233219c05c0b806c0c84dcb60780
Mike Kiernan <mkiernan@microsoft.com>
2017-04-18 10:18:42 +0400

    add gpu support

--- cn-setup.sh
+++ cn-setup.sh
@@ -13,7 +13,25 @@ fi
 
 # Passed in user created by waagent
 HPC_USER=$1
-HPC_GROUP=$HPC_USER
+#HPC_GROUP=$HPC_USER
+
+# Linux distro detection remains a can of worms, just pass it in here:
+VMIMAGE=$2
+# Or uncomment one of these if running this script by hand. 
+#VMIMAGE="Canonical:UbuntuServer:16.04-LTS"
+#VMIMAGE="Canonical:UbuntuServer:16.10"
+#VMIMAGE="OpenLogic:CentOS-HPC:6.5"
+#VMIMAGE="OpenLogic:CentOS:6.8"
+#VMIMAGE="OpenLogic:CentOS-HPC:7.1"
+#VMIMAGE="OpenLogic:CentOS:7.2"
+#VMIMAGE="OpenLogic:CentOS:7.3"
+#VMIMAGE="RedHat:RHEL:7.3"
+#VMIMAGE="SUSE:SLES-HPC:12-SP2" 
+
+PUBLISHER=`echo $VMIMAGE| awk -F ":" '{print $1}'`
+OFFER=`echo $VMIMAGE| awk -F ":" '{print $2}'`
+SKU=`echo $VMIMAGE| awk -F ":" '{print $3}'`
+
 IPHEADNODE=10.0.0.4
 
 # Shares
@@ -29,7 +47,8 @@ setup_disks()
 	chmod -R 777 $SHARE_HOME
 	chmod -R 777 $SHARE_DATA
 	chmod -R 777 $LOCAL_SCRATCH
-}
+
+} #--- end of setup_disks() ---# 
 
 setup_system_centos72()
 {
@@ -40,22 +59,25 @@ setup_system_centos72()
         echo "* hard memlock unlimited" >> /etc/security/limits.conf
         echo "* soft memlock unlimited" >> /etc/security/limits.conf
 
+
+	yum install -y -q nfs-utils
+
+#	systemctl enable rpcbind
+#	systemctl enable nfs-server
+#	systemctl enable nfs-lock
+#	systemctl enable nfs-idmap
+#	systemctl start rpcbind
+#	systemctl start nfs-server
+#	systemctl start nfs-lock
+#	systemctl start nfs-idmap
+
 	ln -s /opt/intel/impi/5.1.3.181/intel64/bin/ /opt/intel/impi/5.1.3.181/bin
 	ln -s /opt/intel/impi/5.1.3.181/lib64/ /opt/intel/impi/5.1.3.181/lib
 
-	yum install -y -q nfs-utils
-	yum install -y -q libibverbs-utils
-	yum install -y -q infiniband-diags
-	yum install -y -q htop sysstat
+        yum install -y -q sshpass nmap htop sysstat
+        yum install -y -q libibverb-utils infiniband-diags
+        yum install -y -q environment-modules
 
-	systemctl enable rpcbind
-	systemctl enable nfs-server
-	systemctl enable nfs-lock
-	systemctl enable nfs-idmap
-	systemctl start rpcbind
-	systemctl start nfs-server
-	systemctl start nfs-lock
-	systemctl start nfs-idmap
 	echo "$IPHEADNODE:$SHARE_DATA $SHARE_DATA nfs4 rw,retry=5,timeo=60,auto,_netdev 0 0" | tee -a /etc/fstab
 	echo "$IPHEADNODE:$SHARE_HOME $SHARE_HOME nfs4 rw,retry=5,timeo=60,auto,_netdev 0 0" | tee -a /etc/fstab
 	cat /etc/fstab
@@ -66,7 +88,26 @@ setup_system_centos72()
 	ls -lR $SHARE_HOME/$HPC_USER
 	touch $SHARE_HOME/$HPC_USER/hosts/$HOSTNAME
 	echo `hostname -i` >>$SHARE_HOME/$HPC_USER/hosts/$HOSTNAME
-}
+
+} } #--- end of setup_system_centos72() ---#
+
+setup_system_ubuntu1604()
+{
+        echo "* hard memlock unlimited" >> /etc/security/limits.conf
+        echo "* soft memlock unlimited" >> /etc/security/limits.conf
+
+        # do this before rpm's or too slow for the scaleset mounts
+        #apt-get install -y -q rpcbind nfs-kernel-server nfs-common
+        apt-get install -y -q nfs-common
+        #systemctl start nfs-kernel-server.service
+
+        apt-get -y update
+        apt-get -y upgrade
+        apt-get install -y -q sshpass nmap htop wget sysstat
+        apt-get install -y -q libibverb-utils infiniband-diags
+        #apt-get install -y -q environment-modules
+
+} #--- end of setup_system_ubuntu1604() ---#
 
 setup_env()
 {
@@ -75,7 +116,8 @@ setup_env()
 	echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> $SHARE_HOME/$HPC_USER/.bashrc
 	echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> $SHARE_HOME/$HPC_USER/.bashrc
 	echo export I_MPI_DYNAMIC_CONNECTION=0 >> $SHARE_HOME/$HPC_USER/.bashrc
-}
+
+} #--- end of setup_env() ---#
 
 setup_user()
 {
@@ -90,11 +132,65 @@ setup_user()
 
         # Disable tty requirement for sudo
         sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
-}
+
+} #--- end of setup_user() ---#
+
+setup_gpus_ubuntu1604()
+{
+	#-- Detect presence of K80 GPU's and configure 
+	gpus=`lspci | grep "NVIDIA Corporation" | grep "Tesla K80"`
+	ngpus=`echo $gpus | wc -l`
+
+	if [ "$gpus" == "" ]; then
+		echo "No gpu found."
+		return;
+	else
+		echo "$ngpus Tesla K80's found. Configuring...."
+	fi
+
+	CUDA_REPO_PKG=cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+	wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG}
+	sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+	rm -f /tmp/${CUDA_REPO_PKG}
+
+	#sudo apt-get update
+	#sudo apt-get upgrade -y
+	sudo apt-get install cuda-drivers
+	sudo apt-get install cuda
+	echo "export PATH=$PATH:/usr/local/cuda-8.0/bin/" >> ~/.bashrc
+	source ~/.bashrc
+	nvidia-smi
+
+} #--- end of setup_gpus_ubuntu1604() ---#
 
 passwd -l $HPC_USER #-- lock account to prevent treading on homedir changes
 setup_disks
-setup_system_centos72
+
+if [[ $PUBLISHER == "Canonical" && $OFFER == "UbuntuServer" && $SKU == "16.04-LTS" ]]; then
+        setup_system_ubuntu1604()
+	setup_gpus_ubuntu1604()
+elif [[ $PUBLISHER == "Canonical" && $OFFER == "UbuntuServer" && $SKU == "16.10" ]]; then
+        setup_system_ubuntu1604()
+	setup_gpus_ubuntu1604()
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS-HPC" && $SKU == "6.5" ]]; then
+        setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS" && $SKU == "6.8" ]]; then
+        setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS-HPC" && $SKU == "7.1" ]]; then
+        setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS" && $SKU == "7.2" ]]; then
+        setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS" && $SKU == "7.3" ]]; then
+        setup_system_centos72
+elif [[ $PUBLISHER == "RedhHat" && $OFFER == "RHEL" && $SKU == "7.3" ]]; then
+        setup_system_centos72
+elif [[ $PUBLISHER == "SUSE" && $OFFER == "SLES-HPC" && $SKU == "12-SP2" ]]; then
+        setup_system_centos72
+else
+        echo "***** IMAGE $PUBLISHER:$OFFER:$VERSION NOT SUPPORTED *****"
+        exit -1
+fi
+
 setup_user
 setup_env
 date
--- hn-setup.sh
+++ hn-setup.sh
@@ -2,7 +2,7 @@
 #
 # Head-Node Installation Script
 #
-# Tested On: CentOS 7.1, 7.2
+# Tested On: CentOS 7.1, 7.2, Ubuntu 16.04
 #
 echo "##################################################"
 echo "############### Head Node Setup ##################"
@@ -18,7 +18,24 @@ fi
 
 # Passed in user created by waagent
 HPC_USER=$1
-$HPC_GROUP=$HPC_USER
+HPC_GROUP=$HPC_USER
+
+# Linux distro detection remains a can of worms, just pass it in here:
+VMIMAGE=$2 
+# Or uncomment one of these if running this script by hand.
+#VMIMAGE="Canonical:UbuntuServer:16.04-LTS"
+#VMIMAGE="Canonical:UbuntuServer:16.10"
+#VMIMAGE="OpenLogic:CentOS-HPC:6.5"
+#VMIMAGE="OpenLogic:CentOS:6.8"
+#VMIMAGE="OpenLogic:CentOS-HPC:7.1"
+#VMIMAGE="OpenLogic:CentOS:7.2"
+#VMIMAGE="OpenLogic:CentOS:7.3"
+#VMIMAGE="RedHat:RHEL:7.3"
+#VMIMAGE="SUSE:SLES-HPC:12-SP2"
+
+PUBLISHER=`echo $VMIMAGE| awk -F ":" '{print $1}'`
+OFFER=`echo $VMIMAGE| awk -F ":" '{print $2}'`
+SKU=`echo $VMIMAGE| awk -F ":" '{print $3}'`
 
 # Shares 
 SHARE_DATA=/share/data
@@ -41,7 +58,8 @@ setup_disks()
 	echo "$SHARE_DATA $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
 	echo "$SHARE_HOME $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
 #	echo "$LOCAL_SCRATCH $localip.*(rw,sync,no_root_squash,no_all_squash)" | tee -a /etc/exports
-}
+
+} #--- end of setup_disks() ---#
 
 setup_system_centos72()
 {
@@ -67,22 +85,45 @@ setup_system_centos72()
 	ln -s /opt/intel/impi/5.1.3.181/intel64/bin/ /opt/intel/impi/5.1.3.181/bin
 	ln -s /opt/intel/impi/5.1.3.181/lib64/ /opt/intel/impi/5.1.3.181/lib
 
+#-verify
 	wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-
 	rpm -ivh epel-release-7-9.noarch.rpm
-	yum install -y -q sshpass nmap htop
+#-verify
+
+	yum install -y -q sshpass nmap htop sysstat
+	yum install -y -q libibverb-utils infiniband-diags
 	yum install -y -q environment-modules
 	#yum groupinstall -y "X Window System"
 	#npm install -g azure-cli
-}
+
+} #--- end of setup_system_centos72() ---#
+
+setup_system_ubuntu1604()
+{
+        echo "* hard memlock unlimited" >> /etc/security/limits.conf
+        echo "* soft memlock unlimited" >> /etc/security/limits.conf
+
+	# do this before rpm's or too slow for the scaleset mounts
+	apt-get install -y -q rpcbind nfs-kernel-server nfs-common
+	systemctl start nfs-kernel-server.service
+
+	apt-get -y update
+	apt-get -y upgrade
+	apt install -y pip
+	pip install --upgrade-pip
+	apt-get install -y -q sshpass nmap htop wget sysstat
+	apt-get install -y -q libibverb-utils infiniband-diags
+	#apt-get install -y -q environment-modules
+
+} #--- end of setup_system_ubuntu1604() ---#
 
 setup_user()
 {
         # Add User + Group
 #	groupadd -g $HPC_GID $HPC_GROUP
 #	useradd -c "HPC User" -g $HPC_GROUP -m -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
-	# Undo the HOME setup done by waagent ossetup
-	mv -p /home/$HPC_USER $SHARE_HOME
+	# Undo the HOME setup done by waagent ossetup -> move it to NFS share
+	#mv -p /home/$HPC_USER $SHARE_HOME
 	usermod -m -d $SHARE_HOME/$HPC_USER $HPC_USER
 
 	# Don't require password for HPC user sudo
@@ -116,19 +157,20 @@ setup_user()
 #	chown $HPC_USER:$HPC_GROUP $SHARE_SCRATCH
 #	chown $HPC_USER:$HPC_GROUP $SHARE_DATA
 	chown $HPC_USER:$HPC_GROUP $LOCAL_SCRATCH
-}
+
+} #--- end of setup_user() ---#
 
 setup_utilities()
 {
 	mkdir -p $SHARE_HOME/$HPC_USER/bin
 	mkdir -p $SHARE_HOME/$HPC_USER/hosts
-	mkdir -p $SHARE_HOME/$HPC_USER/deploy
+	#mkdir -p $SHARE_HOME/$HPC_USER/deploy
 	chmod 755 $SHARE_HOME/$HPC_USER/hosts
 	chmod 755 $SHARE_HOME/$HPC_USER/deploy
 	chown $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER/bin
 	chown $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER/hosts
-	chown $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER/deploy
-	cp hn-setup.sh cn-setup.sh $SHARE_HOME/$HPC_USER/deploy
+	#chown $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER/deploy
+	#cp hn-setup.sh cn-setup.sh $SHARE_HOME/$HPC_USER/deploy
 	cp clusRun.sh pingpong.sh $SHARE_HOME/$HPC_USER/bin
 	chmod 755 $SHARE_HOME/$HPC_USER/bin/*.sh
 
@@ -136,7 +178,6 @@ setup_utilities()
 	myhost=`hostname -i`
 	sed -i '/\<'$myhost'\>/d' $SHARE_HOME/$HPC_USER/bin/nodeips.txt
 	sed -i '/\<10.0.0.1\>/d' $SHARE_HOME/$HPC_USER/bin/nodeips.txt
-
 #
 # Problem to record scale set node names since the nodes are not up yet. 
 # Workaround to have each scale set node create a file with it's hostname in ~/hosts directory. 
@@ -147,7 +188,7 @@ setup_utilities()
 #	NAMES=`ls $SHARE_HOME/$HPC_USER/hosts`
 #	for NAME in $NAMES; do echo $NAME >> $SHARE_HOME/$HPC_USER/bin/nodenames.txt; done
 
-}
+} #--- end of setup_utilities() ---#
 
 #
 # For later, will use environment modules to load user app environments
@@ -159,7 +200,30 @@ setup_environment_modules()
 
 passwd -l $HPC_USER #-- lock account to prevent treading on homedir changes
 setup_disks
-setup_system_centos72
+
+if [[ $PUBLISHER == "Canonical" && $OFFER == "UbuntuServer" && $SKU == "16.04-LTS" ]]; then
+	setup_system_ubuntu1604()
+elif [[ $PUBLISHER == "Canonical" && $OFFER == "UbuntuServer" && $SKU == "16.10" ]]; then
+	setup_system_ubuntu1604()
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS-HPC" && $SKU == "6.5" ]]; then
+	setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS" && $SKU == "6.8" ]]; then
+	setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS-HPC" && $SKU == "7.1" ]]; then
+	setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS" && $SKU == "7.2" ]]; then
+	setup_system_centos72
+elif [[ $PUBLISHER == "OpenLogic" && $OFFER == "CentOS" && $SKU == "7.3" ]]; then
+	setup_system_centos72
+elif [[ $PUBLISHER == "RedhHat" && $OFFER == "RHEL" && $SKU == "7.3" ]]; then
+	setup_system_centos72
+elif [[ $PUBLISHER == "SUSE" && $OFFER == "SLES-HPC" && $SKU == "12-SP2" ]]; then
+	setup_system_centos72
+else 
+	echo "***** IMAGE $PUBLISHER:$OFFER:$VERSION NOT SUPPORTED *****"
+	exit -1
+fi
+
 setup_user
 setup_utilities
 passwd -u $HPC_USER #-- unlock account
