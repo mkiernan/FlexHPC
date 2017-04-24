93faf9e50b5f3b94c3e92b0b2c774eee6a2397f9
Mike Kiernan <mkiernan@microsoft.com>
2017-04-24 11:47:17 +0200

    update pingpong test script

--- pingpong.sh
+++ pingpong.sh
@@ -5,4 +5,16 @@
 # set name of job
 #PBS -N mpi-pingpong
 source /opt/intel/impi/5.1.3.181/bin64/mpivars.sh
-mpirun -env I_MPI_FABRICS=dapl -env I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -env I_MPI_DYNAMIC_CONNECTION=0 IMB-MPI1 pingpong
+
+CLUSTERMAPFS=/clustermap
+HOSTS=`ls -m $CLUSTERMAPFS/hosts | sed "s/ //g"`
+
+source /opt/intel/impi/5.1.3.181/bin64/mpivars.sh
+mpirun -ppn 1 -n 2 -hosts $HOSTS -env I_MPI_FABRICS=shm:dapl -env I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -env I_MPI_DYNAMIC_CONNECTION=0 hostname
+
+echo "MPI hostname test completed"
+
+#mpirun -env I_MPI_FABRICS=dapl -env I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -env I_MPI_DYNAMIC_CONNECTION=0 IMB-MPI1 pingpong
+mpirun -np 32 -ppn 16 -host $HOSTS -env I_MPI_FABRICS=dapl -env I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -env I_MPI_DYNAMIC_CONNECTION=0 IMB-MPI1 pingpong
+
+echo "MPI pingpong test completed."
